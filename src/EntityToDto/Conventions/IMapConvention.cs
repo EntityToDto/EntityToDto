20b066ebbc9b2fa3ed94f1202f9823c5fe100033
Jesse Gador <jessegador@gmail.com>
2020-07-16 04:15:33 +0800

    Start with conventions

---
+++ src/EntityToDto/Conventions/IMapConvention.cs
@@ -0,0 +1,9 @@
+﻿namespace EntityToDto.Conventions
+{
+    /// <summary>
+    /// Conventions that can be added to or removed from a mapper generator.
+    /// </summary>
+    public interface IMapConvention
+    {
+    }
+}
---
+++ src/EntityToDto/Conventions/PropertyNameMappingConvention.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityToDto.Conventions
+{
+    /// <summary>
+    /// Map between types by their property names.
+    /// Thus so, because C# does not allow the same property name to exist in a type.
+    /// </summary>
+    public class PropertyNameMappingConvention : IMapConvention
+    {
+    }
+}
